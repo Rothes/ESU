91c06d0be59adf4ad736c88574a1b70f4281a058
Rothes <449181985@qq.com>
2024-11-28 10:42:57 +0800

    Allow to add a prefix and suffix

--- bukkit/src/main/kotlin/io/github/rothes/esu/bukkit/module/BetterEventMessagesModule.kt
+++ bukkit/src/main/kotlin/io/github/rothes/esu/bukkit/module/BetterEventMessagesModule.kt
@@ -1,10 +1,13 @@
 package io.github.rothes.esu.bukkit.module
 
 import io.github.rothes.esu.bukkit.plugin
+import io.github.rothes.esu.bukkit.user
+import io.github.rothes.esu.bukkit.user.ConsoleUser
 import io.github.rothes.esu.core.configuration.ConfigurationPart
 import io.github.rothes.esu.core.configuration.serializer.OptionalSerializer
 import io.github.rothes.esu.core.module.configuration.BaseModuleConfiguration
 import io.github.rothes.esu.core.module.configuration.EmptyConfiguration
+import io.github.rothes.esu.core.user.User
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.format.NamedTextColor
 import net.kyori.adventure.text.format.TextColor
@@ -18,8 +21,6 @@ import org.bukkit.event.player.PlayerAdvancementDoneEvent
 import org.bukkit.event.player.PlayerJoinEvent
 import org.bukkit.event.player.PlayerQuitEvent
 import org.spongepowered.configurate.objectmapping.meta.Comment
-import java.util.*
-import kotlin.jvm.java
 
 object BetterEventMessagesModule: BukkitModule<BetterEventMessagesModule.ModuleConfig, EmptyConfiguration>(
     ModuleConfig::class.java, EmptyConfiguration::class.java
@@ -60,19 +61,23 @@ object BetterEventMessagesModule: BukkitModule<BetterEventMessagesModule.ModuleC
             event.message(handle(message, config.message.doneAdvancement))
         }
 
-        private fun handle(message: Component, options: ModuleConfig.Message.MessageOptions): Component? {
-            var ret = message
-            val color = options.color
-            if (color.isPresent) {
-                ret = message.color(color.get())
+        private fun handle(message: Component, modifier: ModuleConfig.Message.MessageModifier): Component? {
+            val handler = { user: User ->
+                user.message(
+                    Component.text()
+                        .append(user.buildMinimessage(modifier.head))
+                        .append(modifier.color?.let { message.color(it) } ?: message)
+                        .append(user.buildMinimessage(modifier.foot))
+                        .build()
+                )
             }
-            if (!options.showInConsole) {
-                Bukkit.getOnlinePlayers().forEach {
-                    it.sendMessage(ret)
-                }
-                return null
+            Bukkit.getOnlinePlayers().forEach {
+                handler(it.user)
             }
-            return ret
+            if (modifier.showInConsole) {
+                handler(ConsoleUser)
+            }
+            return null
         }
 
     }
@@ -86,16 +91,19 @@ If 'show-in-console' is false, only the online players can see the message.""")
     ): BaseModuleConfiguration() {
 
         data class Message(
-            val death: MessageOptions = MessageOptions(Optional.ofNullable(TextColor.fromHexString("#dd9090"))),
-            val doneAdvancement: MessageOptions = MessageOptions(Optional.ofNullable(TextColor.fromHexString("#edde0e"))),
-            val playerJoin: MessageOptions = MessageOptions(Optional.ofNullable(NamedTextColor.GRAY)),
-            val playerQuit: MessageOptions = MessageOptions(Optional.ofNullable(NamedTextColor.GRAY)),
+            val death: MessageModifier = MessageModifier(TextColor.fromHexString("#dd9090")),
+            val doneAdvancement: MessageModifier = MessageModifier(TextColor.fromHexString("#edde0e")),
+            val playerJoin: MessageModifier = MessageModifier(NamedTextColor.GRAY, "<pc>[<pdc>+<pc>] "),
+            val playerQuit: MessageModifier = MessageModifier(NamedTextColor.GRAY, "<pc>[<pdc>-<pc>] "),
         ): ConfigurationPart {
 
-            data class MessageOptions(
-                val color: Optional<TextColor> = Optional.ofNullable(null),
+            data class MessageModifier(
+                val color: TextColor? = null,
+                val head: String = "",
+                val foot: String = "",
                 val showInConsole: Boolean = true
             ): ConfigurationPart
+
         }
 
     }
