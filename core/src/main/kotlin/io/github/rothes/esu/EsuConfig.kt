2dd4e4895dc6b5d35eb272edfc36e021ad9bb0c0
Rothes <449181985@qq.com>
2024-12-01 15:38:58 +0800

    Information of setting up h2 database

--- core/src/main/kotlin/io/github/rothes/esu/EsuConfig.kt
+++ core/src/main/kotlin/io/github/rothes/esu/EsuConfig.kt
@@ -25,6 +25,9 @@ object EsuConfig {
                 "You can specify the path to ESU plugin directory from another server.\n" +
                 "Example: /home/user/server/plugins/ESU")
         val localeSoftLinkPath: Optional<Path> = Optional.empty(),
+        @field:Comment("If you don't have a database server,\n" +
+                "set 'jdbc-driver' to 'io.github.rothes.esu.lib.org.h2.Driver'\n" +
+                "and 'jdbc-url' to 'jdbc:h2:file:./plugins/ESU/h2;MODE=MYSQL'")
         val database: Database = Database(),
         val defaultColorScheme: String = "amethyst",
     ): ConfigurationPart {
