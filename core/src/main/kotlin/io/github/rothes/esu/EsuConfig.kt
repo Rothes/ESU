b5a51b8101d7e09df09577d3060b32bc7b71b443
Rothes <449181985@qq.com>
2024-12-02 19:07:00 +0800

    Move h2 db comment

--- core/src/main/kotlin/io/github/rothes/esu/EsuConfig.kt
+++ core/src/main/kotlin/io/github/rothes/esu/EsuConfig.kt
@@ -25,14 +25,14 @@ object EsuConfig {
                 "You can specify the path to ESU plugin directory from another server.\n" +
                 "Example: /home/user/server/plugins/ESU")
         val localeSoftLinkPath: Optional<Path> = Optional.empty(),
-        @field:Comment("If you don't have a database server,\n" +
-                "set 'jdbc-driver' to 'io.github.rothes.esu.lib.org.h2.Driver'\n" +
-                "and 'jdbc-url' to 'jdbc:h2:file:./plugins/ESU/h2;MODE=MYSQL'")
         val database: Database = Database(),
         val defaultColorScheme: String = "amethyst",
     ): ConfigurationPart {
 
         data class Database(
+            @field:Comment("If you don't have a database server,\n" +
+                    "set 'jdbc-driver' to 'io.github.rothes.esu.lib.org.h2.Driver'\n" +
+                    "and 'jdbc-url' to 'jdbc:h2:file:./plugins/ESU/h2;MODE=MYSQL'")
             val jdbcDriver: String = "com.mysql.jdbc.Driver",
             val jdbcUrl: String = "jdbc:mysql://127.0.0.1:3306/esu",
             val username: String = "root",
