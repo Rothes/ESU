e9d310e01c99d6498937c600da677913e069add5
Rothes <449181985@qq.com>
2025-01-30 15:03:24 +0800

    Support actionbar/title/subtitle/sound in message

---
+++ core/src/main/kotlin/io/github/rothes/esu/core/configuration/serializer/MessageDataSerializer.kt
@@ -0,0 +1,20 @@
+package io.github.rothes.esu.core.configuration.serializer
+
+import io.github.rothes.esu.core.configuration.pojo.MessageData
+import org.spongepowered.configurate.serialize.ScalarSerializer
+import org.spongepowered.configurate.serialize.SerializationException
+import java.lang.reflect.Type
+import java.util.function.Predicate
+
+object MessageDataSerializer: ScalarSerializer<MessageData>(MessageData::class.java) {
+
+    @Throws(SerializationException::class)
+    override fun deserialize(type: Type, obj: Any): MessageData {
+        return MessageData.parse(obj.toString())
+    }
+
+    override fun serialize(data: MessageData, typeSupported: Predicate<Class<*>>): String {
+        return data.string
+    }
+
+}
