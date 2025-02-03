2a10242fd680d8c72abf3d2eaa9286de7d4c5b7f
Rothes <449181985@qq.com>
2025-02-04 03:51:17 +0800

    Support to spoof difficulty

--- bukkit/src/main/kotlin/io/github/rothes/esu/bukkit/module/SpoofServerSettingsModule.kt
+++ bukkit/src/main/kotlin/io/github/rothes/esu/bukkit/module/SpoofServerSettingsModule.kt
@@ -5,10 +5,15 @@ import com.github.retrooper.packetevents.event.PacketListenerAbstract
 import com.github.retrooper.packetevents.event.PacketListenerPriority
 import com.github.retrooper.packetevents.event.PacketSendEvent
 import com.github.retrooper.packetevents.protocol.packettype.PacketType
+import com.github.retrooper.packetevents.wrapper.play.server.WrapperPlayServerDifficulty
+import com.github.retrooper.packetevents.wrapper.play.server.WrapperPlayServerServerData
 import com.github.retrooper.packetevents.wrapper.play.server.WrapperPlayServerUpdateSimulationDistance
 import com.github.retrooper.packetevents.wrapper.play.server.WrapperPlayServerUpdateViewDistance
+import io.github.retrooper.packetevents.util.SpigotConversionUtil
 import io.github.rothes.esu.core.module.configuration.BaseModuleConfiguration
 import io.github.rothes.esu.core.module.configuration.EmptyConfiguration
+import org.bukkit.Difficulty
+import java.util.Optional
 
 object SpoofServerSettingsModule: BukkitModule<SpoofServerSettingsModule.ModuleConfig, EmptyConfiguration>(
     ModuleConfig::class.java, EmptyConfiguration::class.java
@@ -38,6 +43,12 @@ object SpoofServerSettingsModule: BukkitModule<SpoofServerSettingsModule.ModuleC
                         wrapper.simulationDistance = config.simulationDistance
                     }
                 }
+                PacketType.Play.Server.SERVER_DIFFICULTY -> {
+                    val wrapper = WrapperPlayServerDifficulty(event)
+                    if (config.difficulty.isPresent) {
+                        wrapper.difficulty = com.github.retrooper.packetevents.protocol.world.Difficulty.valueOf(config.difficulty.get().name)
+                    }
+                }
             }
         }
     }
@@ -45,5 +56,6 @@ object SpoofServerSettingsModule: BukkitModule<SpoofServerSettingsModule.ModuleC
     data class ModuleConfig(
         val viewDistance: Int = -1,
         val simulationDistance: Int = -1,
+        val difficulty: Optional<Difficulty> = Optional.empty(),
     ): BaseModuleConfiguration()
 }
