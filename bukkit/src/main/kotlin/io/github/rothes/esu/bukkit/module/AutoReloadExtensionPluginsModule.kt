fb63e83eb41c077300faebcff3a0ae582a3616fa
Rothes <449181985@qq.com>
2025-02-05 02:15:27 +0800

    Disable extension plugins right on when ESU is disabling

--- bukkit/src/main/kotlin/io/github/rothes/esu/bukkit/module/AutoReloadExtensionPluginsModule.kt
+++ bukkit/src/main/kotlin/io/github/rothes/esu/bukkit/module/AutoReloadExtensionPluginsModule.kt
@@ -3,6 +3,7 @@ package io.github.rothes.esu.bukkit.module
 import com.rylinaux.plugman.PlugMan
 import io.github.rothes.esu.bukkit.module.AutoReloadExtensionPluginsModule.ModuleConfig
 import io.github.rothes.esu.bukkit.plugin
+import io.github.rothes.esu.core.configuration.ConfigLoader
 import io.github.rothes.esu.core.module.configuration.BaseModuleConfiguration
 import io.github.rothes.esu.core.module.configuration.EmptyConfiguration
 import org.bukkit.Bukkit
@@ -12,6 +13,9 @@ import kotlin.jvm.java
 object AutoReloadExtensionPluginsModule: BukkitModule<ModuleConfig, EmptyConfiguration>(
     ModuleConfig::class.java, EmptyConfiguration::class.java
 ) {
+
+    private lateinit var data: ModuleData
+    private val dataPath = moduleFolder.resolve("data.yml")
 
     override val configLoader: (YamlConfigurationLoader.Builder) -> YamlConfigurationLoader.Builder
         get() = {
@@ -29,8 +33,22 @@ object AutoReloadExtensionPluginsModule: BukkitModule<ModuleConfig, EmptyConfigu
         return super.canUse() && !plugin.initialized && Bukkit.getPluginManager().getPlugin("PlugMan") != null
     }
 
+    override fun reloadConfig() {
+        super.reloadConfig()
+        data = ConfigLoader.load(dataPath)
+    }
+
     @Suppress("DEPRECATION")
     override fun enable() {
+        if (!plugin.enabledHot)
+            return
+        for (plugin in data.pluginsToLoad) {
+            PlugMan.getInstance().pluginUtil.load(plugin)
+        }
+        data.pluginsToLoad.clear()
+    }
+
+    override fun disable() {
         val esuName = plugin.description.name
         val plugins = Bukkit.getPluginManager().plugins.filter {
             val description = it.description
@@ -44,11 +62,15 @@ object AutoReloadExtensionPluginsModule: BukkitModule<ModuleConfig, EmptyConfigu
         }
         plugins.forEach {
             PlugMan.getInstance().pluginUtil.unload(it)
+            data.pluginsToLoad.add(it.name)
         }
-        plugins.reversed().forEach {
-            PlugMan.getInstance().pluginUtil.load(it.name)
-        }
+        data.pluginsToLoad.reverse()
+        ConfigLoader.save(dataPath, data)
     }
+
+    data class ModuleData(
+        val pluginsToLoad: MutableList<String> = arrayListOf(),
+    )
 
     data class ModuleConfig(
         val pluginBlacklist: List<String> = listOf("EsuAddonPlugin"),
