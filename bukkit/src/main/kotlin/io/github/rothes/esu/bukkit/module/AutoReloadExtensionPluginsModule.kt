a5484e350b1aaad8b9183c6ebc09f97a381f3e06
Rothes <449181985@qq.com>
2025-02-25 18:38:54 +0800

    Add kotlin.text.Charsets

--- bukkit/src/main/kotlin/io/github/rothes/esu/bukkit/module/AutoReloadExtensionPluginsModule.kt
+++ bukkit/src/main/kotlin/io/github/rothes/esu/bukkit/module/AutoReloadExtensionPluginsModule.kt
@@ -77,6 +77,7 @@ object AutoReloadExtensionPluginsModule: BukkitModule<ModuleConfig, EmptyConfigu
         // Load the classes those are easily to break the hot plugin update.
         org.spongepowered.configurate.yaml.internal.snakeyaml.emitter.Emitter::class.java.declaredClasses // This may cause break when empty data loaded and saving with flow node
         org.incendo.cloud.parser.flag.FlagContext::class.java
+        Charsets::class.java
     }
 
     data class ModuleData(
