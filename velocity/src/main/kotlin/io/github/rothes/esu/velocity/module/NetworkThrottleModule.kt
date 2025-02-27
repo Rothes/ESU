44354aca3a68a35446ba7beabd93ce8bf217bb3e
Rothes <449181985@qq.com>
2025-02-27 22:13:42 +0800

    Update comments

--- velocity/src/main/kotlin/io/github/rothes/esu/velocity/module/NetworkThrottleModule.kt
+++ velocity/src/main/kotlin/io/github/rothes/esu/velocity/module/NetworkThrottleModule.kt
@@ -48,7 +48,7 @@ object NetworkThrottleModule: VelocityModule<NetworkThrottleModule.ModuleConfig,
                 "NetworkThrottleModule at backend servers enabled is required.")
         val dynamicChunkSendRate: DynamicChunkSendRate = DynamicChunkSendRate(),
         @field:Comment("We can't know exactly what the actual bandwidth rate and packet rate are at netty level.\n" +
-                "You can modify the correction parameters here.")
+                "You can modify the calibration parameters here, for advanced users.")
         val trafficCalibration: TrafficCalibration = TrafficCalibration(),
     ): BaseModuleConfiguration() {
 
@@ -57,14 +57,15 @@ object NetworkThrottleModule: VelocityModule<NetworkThrottleModule.ModuleConfig,
 //            @field:Comment("The total upload bandwidth this server can send. Unit is Kbps.")
 //            val totalUploadBandwidth: Long = 50 * 1024,
             @field:Comment("The upload bandwidth threshold to start the throttle. Unit is Kbps.\n" +
-                    "We will use the value from traffic monitor, so you may have done the\n" +
+                    "We will use the data from traffic monitor, so you may have done the\n" +
                     " traffic-calibration settings.")
             val limitUploadBandwidth: Long = 50 * 1024,
             @field:Comment("Minimum guaranteed rate per player. If server bandwidth hits limitUploadBandwidth,\n" +
-                    "player with outgoing rates above this will be limit chunk sending by one second.\n" +
-                    "It's hard to hit the default value, you should set it lower like 2048 if you have\n" +
+                    "player with outgoing rates above this will be limit chunk sending by one second immediately.\n" +
+                    "This helps reduce the probability of spikes, and distributes the bandwidth evenly.\n" +
+                    "It's not easy to hit the default value, you may set it lower like 1520 if you have\n" +
                     " many players.")
-            val guaranteedBandwidth: Long = 3584,
+            val guaranteedBandwidth: Long = 2048,
         )
 
         data class TrafficCalibration(
