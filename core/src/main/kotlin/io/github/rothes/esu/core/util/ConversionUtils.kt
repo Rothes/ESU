b43ecb5fd49ef67bfa681f80227cb1306dc85a74
Rothes <449181985@qq.com>
2025-05-31 12:14:11 +0800

    LocalDateTime to epochMilli

--- core/src/main/kotlin/io/github/rothes/esu/core/util/ConversionUtils.kt
+++ core/src/main/kotlin/io/github/rothes/esu/core/util/ConversionUtils.kt
@@ -1,12 +1,12 @@
 package io.github.rothes.esu.core.util
 
-import kotlinx.datetime.Instant
-import kotlinx.datetime.TimeZone
-import kotlinx.datetime.toLocalDateTime
+import kotlinx.datetime.*
 
 object ConversionUtils {
 
     val Long.localDateTime
         get() = Instant.fromEpochMilliseconds(this).toLocalDateTime(TimeZone.currentSystemDefault())
+    val LocalDateTime.epochMilli
+        get() = toInstant(TimeZone.currentSystemDefault()).toEpochMilliseconds()
 
 }
