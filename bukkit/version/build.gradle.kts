0201692f8f2272dbade1e922a014fe4d7c95e20c
Rothes <449181985@qq.com>
2025-07-23 03:05:03 +0800

    Cleanup

--- bukkit/version/build.gradle.kts
+++ bukkit/version/build.gradle.kts
@@ -12,23 +12,7 @@ allprojects {
 dependencies {
     val serverVer = rootProject.property("targetMinecraftVersion").toString()
     paperweight.paperDevBundle("$serverVer-R0.1-SNAPSHOT")
-    subprojects.filter {
-        it.parent == project
-    }.forEach {
-        api(project(it.path, configuration = "shadow"))
-    }
 }
-
-//tasks.shadowJar {
-//    subprojects.filter {
-//        it.parent == project
-//    }.forEach {
-//        from(it.components["shadow"])
-////        from(it.sourceSets.main.get().output)
-////        api(project(it.path, configuration = "shadow"))
-//    }
-//}
-
 
 subprojects {
     apply(plugin = "io.papermc.paperweight.userdev")
