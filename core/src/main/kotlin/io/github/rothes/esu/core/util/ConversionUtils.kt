1a2f46ae79093b9465d6586ca5af4f9ee18c5572
Rothes <449181985@qq.com>
2025-08-05 00:39:36 +0800

    Fix compile

--- core/src/main/kotlin/io/github/rothes/esu/core/util/ConversionUtils.kt
+++ core/src/main/kotlin/io/github/rothes/esu/core/util/ConversionUtils.kt
@@ -1,11 +1,13 @@
 package io.github.rothes.esu.core.util
 
 import kotlinx.datetime.*
+import kotlin.time.ExperimentalTime
 
 object ConversionUtils {
 
     val Long.localDateTime
         get() = Instant.fromEpochMilliseconds(this).toLocalDateTime(TimeZone.currentSystemDefault())
+    @OptIn(ExperimentalTime::class)
     val LocalDateTime.epochMilli
         get() = toInstant(TimeZone.currentSystemDefault()).toEpochMilliseconds()
 
