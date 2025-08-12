022588476b6aed121ef33384392a5517c57e1290
Rothes <449181985@qq.com>
2025-08-12 18:41:59 +0800

    Make `moduleEnabled` on top

--- core/src/main/kotlin/io/github/rothes/esu/core/module/configuration/BaseModuleConfiguration.kt
+++ core/src/main/kotlin/io/github/rothes/esu/core/module/configuration/BaseModuleConfiguration.kt
@@ -1,7 +1,9 @@
 package io.github.rothes.esu.core.module.configuration
 
 import io.github.rothes.esu.core.configuration.ConfigurationPart
+import io.github.rothes.esu.core.configuration.meta.MoveToTop
 
 open class BaseModuleConfiguration(
+    @MoveToTop
     val moduleEnabled: Boolean = false,
 ): ConfigurationPart
