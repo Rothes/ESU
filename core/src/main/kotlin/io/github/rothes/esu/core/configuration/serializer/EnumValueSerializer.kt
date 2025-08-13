f148893e710be4300d42cf3d7d6eaf7b35b28a05
Rothes <449181985@qq.com>
2025-08-13 19:08:54 +0800

    Deserialize invalid enum to null

--- core/src/main/kotlin/io/github/rothes/esu/core/configuration/serializer/EnumValueSerializer.kt
+++ core/src/main/kotlin/io/github/rothes/esu/core/configuration/serializer/EnumValueSerializer.kt
@@ -1,25 +1,24 @@
 package io.github.rothes.esu.core.configuration.serializer
 
+import io.github.rothes.esu.core.EsuCore
 import io.github.rothes.esu.lib.org.spongepowered.configurate.serialize.ScalarSerializer
-import io.github.rothes.esu.lib.org.spongepowered.configurate.serialize.SerializationException
 import io.github.rothes.esu.lib.org.spongepowered.configurate.util.EnumLookup
 import io.leangen.geantyref.GenericTypeReflector
 import io.leangen.geantyref.TypeToken
 import java.lang.reflect.Type
 import java.util.function.Predicate
-import kotlin.Any
-import kotlin.Enum
-import kotlin.Throws
 
 object EnumValueSerializer: ScalarSerializer<Enum<*>>(TypeToken.get(Enum::class.java)) {
 
-    @Throws(SerializationException::class)
-    override fun deserialize(type: Type, obj: Any): Enum<*> {
+    override fun deserialize(type: Type, obj: Any): Enum<*>? {
         val string = obj.toString()
         val typeClass = GenericTypeReflector.erase(type).asSubclass(Enum::class.java)
         return EnumLookup.lookupEnum(typeClass, string)
             ?: EnumLookup.lookupEnum(typeClass, string.replace("-", "_"))
-            ?: throw SerializationException("Invalid enum constant provided '$string'.")
+            ?: let {
+                EsuCore.instance.warn("Invalid enum constant provided '$string'. Using null.")
+                null
+            }
     }
 
     public override fun serialize(item: Enum<*>, typeSupported: Predicate<Class<*>>): Any {
