90213f2f0d9e6503c4ca4c1873fd36df0b925bd7
Rothes <449181985@qq.com>
2025-08-19 14:24:42 +0800

    Migrate to  UnsafeUtils

--- core/src/main/kotlin/io/github/rothes/esu/core/util/artifact/injector/UnsafeURLInjector.kt
+++ core/src/main/kotlin/io/github/rothes/esu/core/util/artifact/injector/UnsafeURLInjector.kt
@@ -1,28 +1,19 @@
 package io.github.rothes.esu.core.util.artifact.injector
 
 import io.github.rothes.esu.core.EsuCore
-import sun.misc.Unsafe
-import java.lang.reflect.Field
+import io.github.rothes.esu.core.util.UnsafeUtils.usGet
 import java.net.URL
 import java.net.URLClassLoader
 
 object UnsafeURLInjector: URLInjector {
 
-    private val unsafe: Unsafe = Unsafe::class.java.getDeclaredField("theUnsafe").also {
-        it.isAccessible = true
-    }.get(null) as Unsafe
-    private val ucp: Any = URLClassLoader::class.java.getDeclaredField("ucp").unsafe(EsuCore.instance.javaClass.classLoader)
-    private val path: MutableCollection<URL> = ucp.javaClass.getDeclaredField("path").unsafe(ucp)
-    private val unopenedUrls: MutableCollection<URL> = ucp.javaClass.getDeclaredField("unopenedUrls").unsafe(ucp)
+    private val ucp: Any = URLClassLoader::class.java.getDeclaredField("ucp").usGet(EsuCore.instance.javaClass.classLoader)
+    private val path: MutableCollection<URL> = ucp.javaClass.getDeclaredField("path").usGet(ucp)
+    private val unopenedUrls: MutableCollection<URL> = ucp.javaClass.getDeclaredField("unopenedUrls").usGet(ucp)
 
     override fun addURL(url: URL) {
         path.add(url)
         unopenedUrls.add(url)
     }
 
-    private fun <T> Field.unsafe(obj: Any): T {
-        val offset = unsafe.objectFieldOffset(this)
-        @Suppress("UNCHECKED_CAST")
-        return unsafe.getObject(obj, offset) as T
-    }
 }
