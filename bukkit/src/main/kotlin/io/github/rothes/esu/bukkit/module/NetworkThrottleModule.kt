268fa09e017294c1a94a3be228aca3d2cac6452d
Rothes <449181985@qq.com>
2025-08-27 23:52:40 +0800

    Update comments

--- bukkit/src/main/kotlin/io/github/rothes/esu/bukkit/module/NetworkThrottleModule.kt
+++ bukkit/src/main/kotlin/io/github/rothes/esu/bukkit/module/NetworkThrottleModule.kt
@@ -131,45 +131,60 @@ object NetworkThrottleModule: BukkitModule<NetworkThrottleModule.ModuleConfig, N
     )
 
     data class ModuleConfig(
-        @Comment("Helps to reduce chunk upload bandwidth. Plugin will compress invisible blocks in chunk data packet.\n" +
-                "If necessary, we send a full chunk data again.\n" +
-                "This can save about 50% bandwidth usage in overworld and 30% in nether averagely.\n" +
-                "Make sure you enabled network-compression on proxy or this server.")
+        @Comment("""
+            Helps to reduce chunk upload bandwidth. Plugin will compress invisible blocks in chunk data packet.
+            If necessary, we send a full chunk data again.
+            This can save about 50% bandwidth usage in overworld and 30% in nether averagely.
+            Make sure you have enabled network-compression on proxy or this server.
+            """)
         val chunkDataThrottle: ChunkDataThrottle = ChunkDataThrottle(),
         @Comment("Enable DynamicChunkSendRate. Make sure you have velocity mode on, and installed ESU on velocity.")
         val dynamicChunkSendRate: DynamicChunkSendRate = DynamicChunkSendRate(),
-        @Comment("Adjust the settings the players with high latency to lower value.\n" +
-                "So they won't affect average quality of all players.")
+        @Comment("""
+            Adjust the settings the players with high latency to lower value.
+            So they won't affect average quality of all players.
+            """)
         val highLatencyAdjust: HighLatencyAdjust = HighLatencyAdjust(),
     ): BaseModuleConfiguration() {
 
         data class ChunkDataThrottle(
             val enabled: Boolean = true,
-            @Comment("Plugin will resent whole chunk data if resent block amount exceeds this value.\n" +
-                    "Set it to -1 will never resent chunk but keep updating nearby blocks, \n" +
-                    " 0 to always resent chunks.")
+            @Comment("""
+                Plugin will resent whole chunk data if resent block amount exceeds this value.
+                Set it to -1 will never resent chunk but keep updating nearby blocks, 
+                 0 to always resent chunks.
+                """)
             val thresholdToResentWholeChunk: Int = 448,
-            @Comment("We updates the nearby blocks when a player digs a block immediately.\n" +
-                    "If this is enabled, we will check if the block is in the interaction range\n" +
-                    " of the player with a rough calculation.")
+            @Comment("""
+                We updates the nearby blocks when a player digs a block immediately.
+                If this is enabled, we will check if the block is in the interaction range
+                 of the player with a rough calculation.
+                """)
             val updateOnLegalInteractOnly: Boolean = true,
-            @Comment("How many distance of blocks to update from the center while necessary.")
+            @Comment("How many distance of blocks to update from the center when necessary.")
             val updateDistance: Int = 2,
-            @Comment("The bedrock level(minimal height) is never visible unless you are in void.\n" +
-                    "We would skip the check, and if you don't like it you can enable it.")
+            @Comment("""
+                The bedrock level(minimal height) is never visible unless you are in void.
+                We would skip the check, and if you don't like it you can enable it.
+                """)
             val minimalHeightInvisibleCheck: Boolean = false,
             @Comment("""
                 Same with minimal-height but it's for nether roof. For out-of-the-box, it's true by default.
                 It's highly recommend to set it to FALSE if you don't allow players to get above there.
-                """, overrideOld = ["Same with minimal-height but it's for nether roof."])
+                """,
+                overrideOld = ["Same with minimal-height but it's for nether roof."]
+            )
             val netherRoofInvisibleCheck: Boolean = true,
             @RenamedFrom("single-valued-section-block-list")
             @Comment("""
-                    This feature doesn't support running belong with any other anti-xray plugins.
+                    This feature doesn't support running along with any other anti-xray plugins.
                     You must use the anti-xray here we provide.
                     
                     We will send non-visible blocks to one of the random block in this list.
-                """, overrideOld = ["Plugin will convert chunks with all non-visible blocks to single-valued palette format,\nThis could save a lot of bandwidth. And since we are conflicting with anti-xray things,\nyou can use this for some kind of substitution.\nWe choose a random block from the list and make it of a 16*16*16 chunk section."])
+                    If you don't like to anti-xray, you can set the list to 'bedrock'.
+                """,
+                overrideOld = ["Plugin will convert chunks with all non-visible blocks to single-valued palette format,\nThis could save a lot of bandwidth. And since we are conflicting with anti-xray things,\nyou can use this for some kind of substitution.\nWe choose a random block from the list and make it of a 16*16*16 chunk section."]
+            )
             val antiXrayRandomBlockList: DefaultedLinkedHashMap<String, MutableList<Material>> = DefaultedLinkedHashMap<String, MutableList<Material>>(
                 mutableListOf(Material.BEDROCK)
             ).apply {
