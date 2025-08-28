86d2f19e50678982ee7f05313374d82b130d3fbc
Rothes <449181985@qq.com>
2025-08-28 09:05:02 +0800

    Support Spigot

--- bukkit/version/v1_17_1/src/main/kotlin/io/github/rothes/esu/bukkit/module/optimizations/v1/TicketTypeHandlerImpl.kt
+++ bukkit/version/v1_17_1/src/main/kotlin/io/github/rothes/esu/bukkit/module/optimizations/v1/TicketTypeHandlerImpl.kt
@@ -2,21 +2,34 @@ package io.github.rothes.esu.bukkit.module.optimizations.v1
 
 import io.github.rothes.esu.bukkit.module.optimizations.TicketTypeHandler
 import io.github.rothes.esu.core.util.ReflectionUtils.accessibleGet
+import io.github.rothes.esu.core.util.UnsafeUtils.usLongGetter
+import io.github.rothes.esu.core.util.UnsafeUtils.usLongSetter
 import net.minecraft.server.level.TicketType
+import java.lang.reflect.Modifier
 
 class TicketTypeHandlerImpl: TicketTypeHandler {
+
+    companion object {
+        private val expiryTicksField = TicketType::class.java.declaredFields
+            .first { it.type == Long::class.java && !Modifier.isStatic(it.modifiers) }
+        private val expiryTicksFieldPrivate = Modifier.isPrivate(expiryTicksField.modifiers)
+    }
 
     val map = TicketType::class.java.declaredFields
         .filter { it.type.isAssignableFrom(TicketType::class.java) }
         .map { it.accessibleGet(null) as TicketType<*> }
-        .map { TicketTypeImpl(it) }
+        .map { getTicketType(it) }
         .associateBy { it.name }
 
     override fun getTicketTypeMap(): Map<String, TicketTypeHandler.TicketType> {
         return map
     }
 
-    class TicketTypeImpl(
+    private fun getTicketType(handle: TicketType<*>): TicketTypeHandler.TicketType {
+        return if (expiryTicksFieldPrivate) TicketTypeCBImpl(handle) else TicketTypePaperImpl(handle)
+    }
+
+    class TicketTypePaperImpl(
         override val handle: TicketType<*>,
     ): TicketTypeHandler.TicketType {
 
@@ -28,4 +41,24 @@ class TicketTypeHandlerImpl: TicketTypeHandler {
                 handle.timeout = value
             }
     }
+
+    // On Spigot, it's not public
+    class TicketTypeCBImpl(
+        override val handle: TicketType<*>,
+    ): TicketTypeHandler.TicketType {
+
+        override val name: String = handle.toString()
+
+        override var expiryTicks: Long
+            get() = getter[handle]
+            set(value) {
+                setter[handle] = value
+            }
+
+        companion object {
+            private val getter = expiryTicksField.usLongGetter
+            private val setter = expiryTicksField.usLongSetter
+        }
+    }
+
 }
