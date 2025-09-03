ad4959ebe96ee5eecdb2e49012ec202a5ee2e428
Rothes <449181985@qq.com>
2025-09-04 03:47:01 +0800

    [no ci] Add editChangelog task

--- buildSrc/src/main/kotlin/publish-modrinth.gradle.kts
+++ buildSrc/src/main/kotlin/publish-modrinth.gradle.kts
@@ -1,27 +1,32 @@
 import com.github.jengelman.gradle.plugins.shadow.ShadowJavaPlugin.Companion.shadowJar
+import com.google.gson.FieldNamingPolicy
+import com.google.gson.GsonBuilder
+import com.google.gson.JsonObject
+import com.google.gson.reflect.TypeToken
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody.Companion.toRequestBody
 
 plugins {
     id("com.modrinth.minotaur")
 }
 
-project.modrinth {
-    val projectVersion = project.version as String
-    val isRelease = !projectVersion.contains('-')
+private val projectVersion
+    get() = project.version as String
+private val isRelease
+    get() = !projectVersion.contains('-')
+private val versionNumber
+    get() = if (isRelease) projectVersion else "$projectVersion-b${rootProject.commitsSinceLastTag}"
 
-    val versionNumber: String
-    val versionName: String
-    val changelog: String
-    if (isRelease) {
-        versionNumber = projectVersion
-        versionName = "ESU-${project.name} $versionNumber"
-        changelog = "Changelog waiting for edit..."
+project.modrinth {
+    val changelog = if (isRelease) {
+        "Changelog waiting for edit..."
     } else {
-        val buildNumber = rootProject.commitsSinceLastTag
-        versionNumber = "$projectVersion-b$buildNumber"
-        versionName = "ESU-${project.name} $versionNumber"
         val commitHash = rootProject.latestCommitHash
-        changelog = "[$commitHash](https://github.com/Rothes/ESU/commit/$commitHash): ${rootProject.latestCommitMessage}"
+        "[$commitHash](https://github.com/Rothes/ESU/commit/$commitHash): ${rootProject.latestCommitMessage}"
     }
+    val versionName = "ESU-${project.name} $versionNumber"
 
     token.set(System.getenv("MODRINTH_TOKEN"))
     projectId.set("ESU")
@@ -39,3 +44,44 @@ project.modrinth {
     )
     uploadFile.set(tasks.shadowJar)
 }
+
+tasks.register("editChangelog") {
+    val versionNumber = versionNumber
+    val client = OkHttpClient.Builder().build()
+    val response = client.newCall(
+        Request.Builder()
+            .addHeader("Content-Type", "application/json")
+            .url("https://api.modrinth.com/v2/project/esu/version")
+            .get()
+            .build()
+    ).execute().use { it.body!!.string() }
+
+    val token = System.getenv("MODRINTH_TOKEN") ?: error("No token set")
+    val changelog = System.getenv("CHANGELOG_CONTENT") ?: error("No changelog set")
+    val json = JsonObject().apply {
+        addProperty("changelog", changelog)
+    }.toString()
+    val body = json.toRequestBody("application/json".toMediaType())
+    val versions = GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES).create()
+        .fromJson<List<Version>>(response, object : TypeToken<List<Version>>() {}.type)
+        .filter { it.versionNumber == versionNumber }
+    for (version in versions) {
+        client.newCall(
+            Request.Builder()
+                .addHeader("Content-Type", "application/json")
+                .addHeader("Authorization", token)
+                .url("https://api.modrinth.com/v2/version/${version.id}")
+                .patch(body)
+                .build()
+        ).execute().use { response ->
+            if (!response.isSuccessful) {
+                error("Failed to modify version ${version.id}: ${response.body?.string()}")
+            }
+        }
+    }
+}
+
+data class Version(
+    val id: String,
+    val versionNumber: String,
+)
