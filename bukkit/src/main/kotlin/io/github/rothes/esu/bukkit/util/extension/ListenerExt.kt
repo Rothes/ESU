01fca8aa50a5da31147343f159fe36e2d6649134
Rothes <449181985@qq.com>
2025-09-17 20:04:57 +0800

    Add `Listener.register`

--- bukkit/src/main/kotlin/io/github/rothes/esu/bukkit/util/extension/ListenerExt.kt
+++ bukkit/src/main/kotlin/io/github/rothes/esu/bukkit/util/extension/ListenerExt.kt
@@ -1,9 +1,15 @@
 package io.github.rothes.esu.bukkit.util.extension
 
+import org.bukkit.Bukkit
 import org.bukkit.event.HandlerList
 import org.bukkit.event.Listener
+import org.bukkit.plugin.Plugin
 
 object ListenerExt {
+
+    fun Listener.register(plugin: Plugin = io.github.rothes.esu.bukkit.plugin) {
+        Bukkit.getPluginManager().registerEvents(this, plugin)
+    }
 
     fun Listener.unregister() {
         HandlerList.unregisterAll(this)
