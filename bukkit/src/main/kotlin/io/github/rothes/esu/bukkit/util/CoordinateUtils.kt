47b8be2b3dcd5c39e1d666a9cb5631b3f18aefd7
Rothes <449181985@qq.com>
2025-09-18 04:16:20 +0800

    Add getBlockKey functions

--- bukkit/src/main/kotlin/io/github/rothes/esu/bukkit/util/CoordinateUtils.kt
+++ bukkit/src/main/kotlin/io/github/rothes/esu/bukkit/util/CoordinateUtils.kt
@@ -23,7 +23,7 @@ object CoordinateUtils {
     }
     val Block.blockChunkKey
         get() = getBlockChunkKey(x, y, z)
-    
+
     val Int.blockChunkX: Int
         get() = this and 0xf
     val Int.blockChunkY: Int
@@ -33,7 +33,27 @@ object CoordinateUtils {
     val Int.blockChunkPos
         get() = BlockChunkPos(blockChunkX, blockChunkY, blockChunkZ)
 
+    /**
+     * x, z: (-67108864 .. 67108863)
+     * y: (-512 .. 511)
+     */
+    fun getBlockKey(x: Int, y: Int, z: Int): Long {
+        return (x.toLong() and 0x7FFFFFFL) or ((z.toLong() and 0x7FFFFFFL) shl 27) or (y.toLong() shl 54)
+    }
+    val Block.blockKey_: Long
+        get() = getBlockKey(x, y, z)
+
+    val Long.blockX: Int
+        get() = (this shl 37 shr 37).toInt() // Use shl+shr to keep sign
+    val Long.blockY: Int
+        get() = (this shr 54).toInt()
+    val Long.blockZ: Int
+        get() = (this shl 10 shr 37).toInt()
+    val Long.blockPos: BlockPos
+        get() = BlockPos(blockX, blockY, blockZ)
+
     data class ChunkPos(val x: Int, val z: Int)
     data class BlockChunkPos(val x: Int, val y: Int, val z: Int)
+    data class BlockPos(val x: Int, val y: Int, val z: Int)
 
 }
