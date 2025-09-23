a3197abc384f0c59e38af51ffba93c5fdb637994
Rothes <449181985@qq.com>
2025-09-24 04:42:12 +0800

    Manually send chunk packet on 1.20.2+

--- bukkit/version/v1_20_2/src/main/kotlin/io/github/rothes/esu/bukkit/module/networkthrottle/chunkdatathrottle/v1_20_2/ChunkSenderImpl.kt
+++ bukkit/version/v1_20_2/src/main/kotlin/io/github/rothes/esu/bukkit/module/networkthrottle/chunkdatathrottle/v1_20_2/ChunkSenderImpl.kt
@@ -1,18 +1,23 @@
 package io.github.rothes.esu.bukkit.module.networkthrottle.chunkdatathrottle.v1_20_2
 
 import io.github.rothes.esu.bukkit.module.networkthrottle.chunkdatathrottle.ChunkSender
-import io.github.rothes.esu.bukkit.util.scheduler.Scheduler
+import net.minecraft.network.protocol.game.ClientboundLevelChunkWithLightPacket
 import net.minecraft.server.level.ServerLevel
 import net.minecraft.server.level.ServerPlayer
-import net.minecraft.server.network.PlayerChunkSender
 import net.minecraft.world.level.chunk.LevelChunk
 
 class ChunkSenderImpl: ChunkSender {
 
     override fun sendChunk(player: ServerPlayer, level: ServerLevel, chunk: LevelChunk) {
-        Scheduler.schedule(player.bukkitEntity) {
-            PlayerChunkSender.sendChunk(player.connection, level, chunk)
-        }
+        @Suppress("DEPRECATION") // Spigot support
+        player.connection.send(
+            ClientboundLevelChunkWithLightPacket(
+                chunk,
+                level.lightEngine,
+                null,
+                null
+            )
+        )
     }
 
 }
