11fa7dbfbef604f5ee56b12f5bc1bdf0da033726
Rothes <449181985@qq.com>
2025-09-24 03:54:54 +0800

    SendChunk on current thread

--- bukkit/version/v1_20_2/src/main/kotlin/io/github/rothes/esu/bukkit/module/networkthrottle/chunkdatathrottle/v1_20_2/ChunkSenderImpl.kt
+++ bukkit/version/v1_20_2/src/main/kotlin/io/github/rothes/esu/bukkit/module/networkthrottle/chunkdatathrottle/v1_20_2/ChunkSenderImpl.kt
@@ -1,6 +1,7 @@
 package io.github.rothes.esu.bukkit.module.networkthrottle.chunkdatathrottle.v1_20_2
 
 import io.github.rothes.esu.bukkit.module.networkthrottle.chunkdatathrottle.ChunkSender
+import io.github.rothes.esu.bukkit.util.scheduler.Scheduler
 import net.minecraft.server.level.ServerLevel
 import net.minecraft.server.level.ServerPlayer
 import net.minecraft.server.network.PlayerChunkSender
@@ -9,7 +10,9 @@ import net.minecraft.world.level.chunk.LevelChunk
 class ChunkSenderImpl: ChunkSender {
 
     override fun sendChunk(player: ServerPlayer, level: ServerLevel, chunk: LevelChunk) {
-        PlayerChunkSender.sendChunk(player.connection, level, chunk)
+        Scheduler.schedule(player.bukkitEntity) {
+            PlayerChunkSender.sendChunk(player.connection, level, chunk)
+        }
     }
 
 }
