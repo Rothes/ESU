3964f890a02a55afd2279894d8d2c831dd000859
Rothes <449181985@qq.com>
2025-10-28 21:44:21 +0800

    Fix nested toggleByAvailable triggering

--- core/src/main/kotlin/io/github/rothes/esu/core/module/Feature.kt
+++ core/src/main/kotlin/io/github/rothes/esu/core/module/Feature.kt
@@ -21,28 +21,23 @@ interface Feature<C: ConfigurationPart, L: ConfigurationPart> {
     val lang: MultiLangConfiguration<L>
 
     fun setConfigInstance(instance: C) {}
-    fun setEnabled(value: Boolean) {}
+    fun setEnabled(value: Boolean) {
+        // Notify children
+        for (feature in getFeatures()) {
+            feature.toggleByAvailable()
+        }
+    }
     fun setParent(parent: Feature<*, *>?) {}
 
     fun toggleByAvailable(): AvailableCheck {
         val available = isAvailable()
 
-        var changed = false
         if (available.value && !enabled) {
             onEnable()
             setEnabled(true)
-            changed = true
         } else if (!available.value && enabled) {
             setEnabled(false)
             onDisable()
-            changed = true
-        }
-
-        if (changed) {
-            // Notify children
-            for (feature in getFeatures()) {
-                feature.toggleByAvailable()
-            }
         }
 
         return available
