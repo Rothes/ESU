2a46225aab426a79c61ad0857d9594738bb5498c
Rothes <449181985@qq.com>
2025-11-03 02:36:02 +0800

    [EntityCulling] Optimize entity storage

--- bukkit/src/main/kotlin/io/github/rothes/esu/bukkit/module/networkthrottle/entityculling/UserCullData.kt
+++ bukkit/src/main/kotlin/io/github/rothes/esu/bukkit/module/networkthrottle/entityculling/UserCullData.kt
@@ -4,6 +4,7 @@ import io.github.rothes.esu.bukkit.bootstrap
 import io.github.rothes.esu.bukkit.plugin
 import io.github.rothes.esu.bukkit.util.scheduler.Scheduler
 import io.github.rothes.esu.bukkit.util.version.adapter.TickThreadAdapter.Companion.checkTickThread
+import it.unimi.dsi.fastutil.Hash
 import it.unimi.dsi.fastutil.ints.Int2ReferenceOpenHashMap
 import it.unimi.dsi.fastutil.ints.IntArrayList
 import org.bukkit.entity.Entity
@@ -13,51 +14,45 @@ class UserCullData(
     val player: Player,
 ) {
 
-    private val hiddenEntities = Int2ReferenceOpenHashMap<Entity>()
+    private val hiddenEntities = Int2ReferenceOpenHashMap<Entity>(32, Hash.FAST_LOAD_FACTOR)
     private val pendingChanges = mutableListOf<CulledChange>()
     private var tickedTime = 0
-    private var markedReset = false
     private var isRemoved = false
     private var terminated = false
 
-    fun hiddenEntities(): List<Entity> {
-        return hiddenEntities.values.toList()
-    }
-
     fun setCulled(entity: Entity, entityId: Int, culled: Boolean, pend: Boolean = true) {
         if (culled) {
-            if (hiddenEntities.put(entityId, entity) == null && pend)
-                pendCulledChange(entity, entityId, true)
+            synchronized(hiddenEntities) {
+                if (hiddenEntities.put(entityId, entity) == null && pend)
+                    pendCulledChange(entity, entityId, true)
+            }
         } else {
-            if (hiddenEntities.remove(entityId) != null && pend)
-                pendCulledChange(entity, entityId, false)
+            synchronized(hiddenEntities) {
+                if (hiddenEntities.remove(entityId) != null && pend)
+                    pendCulledChange(entity, entityId, false)
+            }
         }
     }
 
     fun showAll() {
-        if (isRemoved) {
-            reset()
-            updateChanges()
-            terminated = true
-        } else {
-            markedReset = true
-        }
+        reset()
+        updateChanges()
     }
 
     fun tick() {
         if (++tickedTime >= 60 * 2 * 20) {
             checkEntitiesValid()
         }
-        if (markedReset)
-            reset()
         updateChanges()
     }
 
     fun onEntityRemove(entities: IntArrayList) {
         if (entities.isEmpty) return
         val iterator = entities.listIterator()
-        while (iterator.hasNext()) {
-            hiddenEntities.remove(iterator.nextInt())
+        synchronized(hiddenEntities) {
+            while (iterator.hasNext()) {
+                hiddenEntities.remove(iterator.nextInt())
+            }
         }
     }
 
@@ -66,22 +61,26 @@ class UserCullData(
     }
 
     private fun reset() {
-        val iterator = hiddenEntities.int2ReferenceEntrySet().iterator()
-        for (entry in iterator) {
-            val id = entry.intKey
-            val entity = entry.value
-            iterator.remove()
-            pendingChanges.add(CulledChange(entity, id, false))
+        synchronized(hiddenEntities) {
+            val iterator = hiddenEntities.int2ReferenceEntrySet().iterator()
+            for (entry in iterator) {
+                val id = entry.intKey
+                val entity = entry.value
+                iterator.remove()
+                pendCulledChange(entity, id, false)
+            }
         }
     }
 
     private fun checkEntitiesValid() {
         try {
-            val iterator = hiddenEntities.int2ReferenceEntrySet().iterator()
-            for (entry in iterator) {
-                val entity = entry.value
-                if (entity.isDead) {
-                    iterator.remove()
+            synchronized(hiddenEntities) {
+                val iterator = hiddenEntities.int2ReferenceEntrySet().iterator()
+                for (entry in iterator) {
+                    val entity = entry.value
+                    if (entity.isDead) {
+                        iterator.remove()
+                    }
                 }
             }
         } catch (e: Throwable) {
@@ -101,7 +100,8 @@ class UserCullData(
                     if (change.entity.isDead) continue
                     if (!change.entity.checkTickThread()) {
                         // Not on tick thread, we can only roll state back
-                        setCulled(change.entity, change.entityId, !change.culled, false)
+                        if (!change.culled)
+                            setCulled(change.entity, change.entityId, true, pend = false)
                         continue
                     }
                     if (change.culled)
