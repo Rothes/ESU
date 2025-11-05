050a6233af44e4dceb8600d51cb31bcc87e1ca10
Rothes <449181985@qq.com>
2025-11-05 15:13:52 +0800

    Still use viewDistanceSquared

--- bukkit/version/v1_18/src/main/kotlin/io/github/rothes/esu/bukkit/module/networkthrottle/entityculling/v1_18/RaytraceHandlerImpl.kt
+++ bukkit/version/v1_18/src/main/kotlin/io/github/rothes/esu/bukkit/module/networkthrottle/entityculling/v1_18/RaytraceHandlerImpl.kt
@@ -49,10 +49,7 @@ import org.incendo.cloud.annotations.Command
 import org.spigotmc.TrackingRange
 import java.util.concurrent.Executors
 import java.util.concurrent.atomic.AtomicInteger
-import kotlin.math.abs
-import kotlin.math.floor
-import kotlin.math.sign
-import kotlin.math.sqrt
+import kotlin.math.*
 import kotlin.time.Duration.Companion.seconds
 
 class RaytraceHandlerImpl: RaytraceHandler<RaytraceHandlerImpl.RaytraceConfig, EmptyConfiguration>() {
@@ -277,6 +274,8 @@ class RaytraceHandlerImpl: RaytraceHandler<RaytraceHandlerImpl.RaytraceConfig, E
     }
 
     fun tickPlayer(player: ServerPlayer, bukkit: Player, userCullData: UserCullData, level: ServerLevel, entities: Iterable<Entity>) {
+        val viewDistanceSquared = (bukkit.viewDistance + 1).square() shl 8
+
         val predicatedPlayerPos = if (userCullData.shouldCull && config.predicatePlayerPositon) {
             val velocity = playerVelocityGetter.getPlayerMoveVelocity(player)
             if (velocity.lengthSqr() >= 0.06) { // Threshold for sprinting
@@ -309,9 +308,10 @@ class RaytraceHandlerImpl: RaytraceHandler<RaytraceHandlerImpl.RaytraceConfig, E
             if (entity == player) continue
 
             // Get tracking range for this entity type, adding extra 1 chunk to it.
-            val range = (TrackingRange.getEntityTrackingRange(entity, entity.type.clientTrackingRange() shl 4) + 16).square()
+            val typeRange = (TrackingRange.getEntityTrackingRange(entity, entity.type.clientTrackingRange() shl 4) + 16).square()
+            val finalRange = min(typeRange, viewDistanceSquared)
             val dist = (player.x - entity.x).square() + (player.z - entity.z).square()
-            if (dist > range) continue
+            if (dist > finalRange) continue
 
             tickedEntities++
 
