133beccaaad3aa85c511711b3cd54a057b5e2fce
Rothes <449181985@qq.com>
2025-11-06 15:04:29 +0800

    Fix EntityUpdateInterval feature name

--- bukkit/src/main/kotlin/io/github/rothes/esu/bukkit/module/networkthrottle/EntityUpdateInterval.kt
+++ bukkit/src/main/kotlin/io/github/rothes/esu/bukkit/module/networkthrottle/EntityUpdateInterval.kt
@@ -2,4 +2,8 @@ package io.github.rothes.esu.bukkit.module.networkthrottle
 
 import io.github.rothes.esu.core.module.CommonFeature
 
-abstract class EntityUpdateInterval<C, L>: CommonFeature<C, L>()
+abstract class EntityUpdateInterval<C, L>: CommonFeature<C, L>() {
+
+    override val name: String = "EntityUpdateInterval"
+
+}
