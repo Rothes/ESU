2cf9d555875b5cd15aa17d965b670cdfaab4401b
Rothes <449181985@qq.com>
2025-11-17 14:17:25 +0800

    Improve module override

--- bukkit/src/main/kotlin/io/github/rothes/esu/bukkit/module/essencialcommands/BaseCommand.kt
+++ bukkit/src/main/kotlin/io/github/rothes/esu/bukkit/module/essencialcommands/BaseCommand.kt
@@ -6,6 +6,7 @@ import io.github.rothes.esu.core.module.CommonFeature
 abstract class BaseCommand<C, L> : CommonFeature<C, L>() {
 
     override val name: String = javaClass.simpleName.removeSuffix("Command")
-    override val module: EssentialCommandsModule = EssentialCommandsModule
+    override val module: EssentialCommandsModule
+        get() = super.module as EssentialCommandsModule
 
 }
