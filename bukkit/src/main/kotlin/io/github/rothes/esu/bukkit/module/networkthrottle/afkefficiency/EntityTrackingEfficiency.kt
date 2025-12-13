136cb0c0f7662dd0e88b05e21f6340009986ee3b
Rothes <449181985@qq.com>
2025-12-13 21:44:00 +0800

    [EntityTrackingEfficiency] Improve performance & mem usage

--- bukkit/src/main/kotlin/io/github/rothes/esu/bukkit/module/networkthrottle/afkefficiency/EntityTrackingEfficiency.kt
+++ bukkit/src/main/kotlin/io/github/rothes/esu/bukkit/module/networkthrottle/afkefficiency/EntityTrackingEfficiency.kt
@@ -2,13 +2,17 @@ package io.github.rothes.esu.bukkit.module.networkthrottle.afkefficiency
 
 import io.github.rothes.esu.bukkit.bootstrap
 import io.github.rothes.esu.bukkit.module.networkthrottle.AfkEfficiency
-import io.github.rothes.esu.bukkit.util.PlayerEntityVisibilityHolder
+import io.github.rothes.esu.bukkit.module.networkthrottle.entityculling.PlayerEntityVisibilityHandler
 import io.github.rothes.esu.bukkit.util.ServerCompatibility
 import io.github.rothes.esu.bukkit.util.extension.ListenerExt.register
 import io.github.rothes.esu.bukkit.util.extension.ListenerExt.unregister
 import io.github.rothes.esu.bukkit.util.extension.createChild
 import io.github.rothes.esu.bukkit.util.scheduler.ScheduledTask
 import io.github.rothes.esu.bukkit.util.scheduler.Scheduler.delayedTick
+import io.github.rothes.esu.bukkit.util.scheduler.Scheduler.syncTick
+import io.github.rothes.esu.bukkit.util.version.Versioned
+import io.github.rothes.esu.bukkit.util.version.adapter.TickThreadAdapter.Companion.checkTickThread
+import io.github.rothes.esu.bukkit.util.version.adapter.nms.EntityValidTester
 import io.github.rothes.esu.core.configuration.data.MessageData.Companion.message
 import io.github.rothes.esu.core.configuration.meta.Comment
 import io.github.rothes.esu.core.module.Feature
@@ -16,9 +20,11 @@ import io.github.rothes.esu.core.module.Feature.AvailableCheck.Companion.errFail
 import io.github.rothes.esu.core.module.configuration.BaseFeatureConfiguration
 import io.github.rothes.esu.core.util.extension.math.square
 import io.papermc.paper.event.player.PlayerTrackEntityEvent
+import org.bukkit.entity.Entity
 import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
+import java.util.*
 import java.util.concurrent.ConcurrentHashMap
 import java.util.logging.Level
 
@@ -58,24 +64,6 @@ object EntityTrackingEfficiency: AfkEfficiencyFeature<EntityTrackingEfficiency.F
         playerData.remove(playerHolder.player)?.shutdown()
     }
 
-    private fun updatePlayer(player: Player, data: EfficiencyData) {
-        val loc = player.location
-        val dist = config.visibleEntityDistanceSquared
-        for (chunk in player.sentChunks) {
-            val entities = chunk.entities
-            for (entity in entities) {
-                if (entity is Player) continue
-                if (entity.location.distanceSquared(loc) <= dist) {
-                    if (data.hiddenHolder.map.remove(entity.entityId) != null)
-                        player.showEntity(pl, entity)
-                } else {
-                    if (data.hiddenHolder.map.put(entity.entityId, entity) == null)
-                        player.hideEntity(pl, entity)
-                }
-            }
-        }
-    }
-
     private object Listeners: Listener {
 
         @EventHandler
@@ -86,7 +74,7 @@ object EntityTrackingEfficiency: AfkEfficiencyFeature<EntityTrackingEfficiency.F
             if (player.location.distanceSquared(entity.location) < config.visibleEntityDistanceSquared) return
 
             val data = playerData[player] ?: return
-            data.hiddenHolder.map.put(entity.entityId, entity)
+            data.hiddenEntities.add(entity)
             player.hideEntity(pl, entity) // Call hideEntity otherwise showEntity doesn't re-track the entity.
             e.isCancelled = true
         }
@@ -94,27 +82,93 @@ object EntityTrackingEfficiency: AfkEfficiencyFeature<EntityTrackingEfficiency.F
 
     private class EfficiencyData(val player: Player) {
 
+        companion object {
+            private val VISIBILITY_HANDLER by Versioned(PlayerEntityVisibilityHandler::class.java)
+            private val VALID_TESTER by Versioned(EntityValidTester::class.java)
+        }
+
         @Volatile private var shutdown = false
         private var ticked = 0L
         private var task: ScheduledTask = schedule()
 
-        val hiddenHolder = PlayerEntityVisibilityHolder(player, pl)
+        val hiddenEntities = LinkedList<Entity>()
+
+        init {
+            start()
+        }
+
+        fun start() {
+            player.syncTick {
+                val loc = player.location
+                val dist = config.visibleEntityDistanceSquared
+                for (chunk in player.sentChunks) {
+                    val entities = chunk.entities
+                    for (entity in entities) {
+                        if (entity is Player) continue
+                        if (entity.location.distanceSquared(loc) > dist) {
+                            hiddenEntities.add(entity)
+                            player.hideEntity(pl, entity)
+                        }
+                    }
+                }
+            }
+        }
+
+        fun update() {
+            val loc = player.location
+            val visibleDist = config.visibleEntityDistanceSquared
+            val iterator = hiddenEntities.iterator()
+            for (entity in iterator) {
+                if (!VALID_TESTER.isValid(entity)) {
+                    // The entity is removed from the world
+                    iterator.remove()
+                    continue
+                }
+
+                val other = entity.location
+                if (other.world !== loc.world) {
+                    // Entity is not on same world anymore
+                    VISIBILITY_HANDLER.forceShowEntity(player, entity, pl)
+                } else {
+                    val dist = entity.location.distanceSquared(loc)
+                    if (dist <= visibleDist) {
+                        player.showEntity(pl, entity)
+                    } else if (dist > 1024 * 1024) {
+                        // Entity is far away
+                        if (entity.checkTickThread()) {
+                            // May be just huge Y diff, still need to use this to re-track entity
+                            player.showEntity(pl, entity)
+                        } else {
+                            VISIBILITY_HANDLER.forceShowEntity(player, entity, pl)
+                        }
+                    } else {
+                        // Still hide this entity
+                        continue
+                    }
+                }
+                iterator.remove()
+            }
+        }
 
         fun shutdown() {
             shutdown = true
             task.cancel()
-            hiddenHolder.showAll()
+            for (entity in hiddenEntities) {
+                if (VALID_TESTER.isValid(entity)) {
+                    if (entity.checkTickThread())
+                        player.showEntity(pl, entity)
+                    else
+                        VISIBILITY_HANDLER.forceShowEntity(player, entity, pl)
+                }
+            }
+            hiddenEntities.clear()
         }
 
         private fun schedule(): ScheduledTask {
             fun tick() {
                 if (shutdown) return
                 try {
-                    updatePlayer(player, this)
-                    if (ticked >= config.invalidateValidTicks) {
-                        hiddenHolder.checkEntitiesValid()
-                        ticked = 0
-                    }
+                    update()
                 } catch (e: Throwable) {
                     pl.logger.log(Level.SEVERE, "Failed to update ${player.name}", e)
                 }
@@ -140,8 +194,6 @@ object EntityTrackingEfficiency: AfkEfficiencyFeature<EntityTrackingEfficiency.F
         val visibleEntityDistance: Double = 7.0,
         @Comment("Server game tick interval to update player nearby entities")
         val updateIntervalTicks: Long = 5,
-        @Comment("Server game tick interval to purge player hidden entities cache")
-        val invalidateValidTicks: Long = 200,
     ): BaseFeatureConfiguration(true) {
 
         val visibleEntityDistanceSquared by lazy { visibleEntityDistance.square() }
