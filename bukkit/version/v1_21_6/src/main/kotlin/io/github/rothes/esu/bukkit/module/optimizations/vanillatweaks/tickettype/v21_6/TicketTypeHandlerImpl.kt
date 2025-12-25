8865632563d8895cd6b72c4484884f19dbff9144
Rothes <449181985@qq.com>
2025-12-26 02:27:44 +0800

    Support TicketType on Spigot 1.21.6+

--- bukkit/version/v1_21_6/src/main/kotlin/io/github/rothes/esu/bukkit/module/optimizations/vanillatweaks/tickettype/v21_6/TicketTypeHandlerImpl.kt
+++ bukkit/version/v1_21_6/src/main/kotlin/io/github/rothes/esu/bukkit/module/optimizations/vanillatweaks/tickettype/v21_6/TicketTypeHandlerImpl.kt
@@ -1,8 +1,10 @@
-package io.github.rothes.esu.bukkit.module.optimizations.vanillatweaks.tickettype.v21_6__paper
+package io.github.rothes.esu.bukkit.module.optimizations.vanillatweaks.tickettype.v21_6
 
 import io.github.rothes.esu.bukkit.module.optimizations.vanillatweaks.tickettype.TicketTypeHandler
+import io.github.rothes.esu.bukkit.util.ServerCompatibility
 import io.github.rothes.esu.bukkit.util.version.Versioned
 import io.github.rothes.esu.bukkit.util.version.adapter.nms.ResourceKeyHandler
+import io.github.rothes.esu.core.util.UnsafeUtils.usLongAccessor
 import net.minecraft.core.registries.BuiltInRegistries
 import net.minecraft.server.level.TicketType
 
@@ -14,11 +16,15 @@ object TicketTypeHandlerImpl: TicketTypeHandler {
         .entrySet()
         .associate {
             val name = KEY_HANDLER.getResourceKeyString(it.key)
-            name to NmsTicketTypeMoonriseImpl(it.value, name)
+            name to wrapTicketType(it.value, name)
         }
 
     override fun getTicketTypeMap(): Map<String, TicketTypeHandler.NmsTicketType> {
         return map
+    }
+
+    private fun wrapTicketType(handle: TicketType<*>, name: String): TicketTypeHandler.NmsTicketType {
+        return if (ServerCompatibility.isPaper) NmsTicketTypeMoonriseImpl(handle, name) else NmsTicketTypeCBImpl(handle, name)
     }
 
     class NmsTicketTypeMoonriseImpl(
@@ -31,6 +37,22 @@ object TicketTypeHandlerImpl: TicketTypeHandler {
             set(value) {
                 handle.`moonrise$setTimeout`(value)
             }
+    }
+
+    class NmsTicketTypeCBImpl(
+        override val handle: TicketType<*>,
+        override val name: String,
+    ): TicketTypeHandler.NmsTicketType {
+
+        override var expiryTicks: Long
+            get() = handle.timeout()
+            set(value) {
+                TIMEOUT[handle] = value
+            }
+
+        companion object {
+            private val TIMEOUT = TicketType::class.java.getDeclaredField("timeout").usLongAccessor
+        }
     }
 
 }
