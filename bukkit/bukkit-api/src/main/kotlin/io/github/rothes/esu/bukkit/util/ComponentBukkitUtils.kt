c7d6a2b3def087bb5b269503e2d5743d06ef515b
Rothes <449181985@qq.com>
2026-01-02 00:40:26 +0800

    Support lagacy papi

--- bukkit/bukkit-api/src/main/kotlin/io/github/rothes/esu/bukkit/util/ComponentBukkitUtils.kt
+++ bukkit/bukkit-api/src/main/kotlin/io/github/rothes/esu/bukkit/util/ComponentBukkitUtils.kt
@@ -6,6 +6,7 @@ import io.github.rothes.esu.bukkit.util.version.adapter.PlayerAdapter.Companion.
 import io.github.rothes.esu.core.user.User
 import io.github.rothes.esu.core.util.AdventureConverter.esu
 import io.github.rothes.esu.core.util.ComponentUtils.component
+import io.github.rothes.esu.core.util.ComponentUtils.legacy
 import io.github.rothes.esu.core.util.ComponentUtils.unparsed
 import io.github.rothes.esu.lib.adventure.text.Component
 import io.github.rothes.esu.lib.adventure.text.minimessage.tag.Tag
@@ -44,12 +45,13 @@ object ComponentBukkitUtils {
                 val split = papi.split('_', limit = 2)
                 val expansion = PlaceholderAPIPlugin.getInstance().localExpansionManager.getExpansion(split[0].lowercase())
                     ?: return@resolver Tag.inserting(Component.text(papi))
-                val result = expansion.onRequest(player, split.getOrElse(1) { "" })
+                val result = expansion.onRequest(player, split.getOrElse(1) { "" }) ?: papi
 
                 val type = if (arg.hasNext()) arg.pop().lowerValue() else "plain"
                 when (type) {
-                    "plain" -> Tag.inserting(Component.text(result ?: papi))
-                    "minimessage" -> Tag.inserting(context.deserialize(result ?: papi))
+                    "plain" -> Tag.inserting(Component.text(result))
+                    "legacy" -> Tag.inserting(result.legacy)
+                    "minimessage" -> Tag.inserting(context.deserialize(result))
                     else -> error("Unknown text type $type")
                 }
 
