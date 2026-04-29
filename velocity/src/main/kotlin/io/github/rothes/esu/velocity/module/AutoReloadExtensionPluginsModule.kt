17837c1464b4fb367d220b443494c6380de96b47
Rothes <449181985@qq.com>
2026-04-29 10:07:51 +0800

    [AutoReloadExtensionPlugins] Fix

--- velocity/src/main/kotlin/io/github/rothes/esu/velocity/module/AutoReloadExtensionPluginsModule.kt
+++ velocity/src/main/kotlin/io/github/rothes/esu/velocity/module/AutoReloadExtensionPluginsModule.kt
@@ -2,14 +2,19 @@ package io.github.rothes.esu.velocity.module
 
 import io.github.rothes.esu.core.configuration.ConfigLoader
 import io.github.rothes.esu.core.configuration.data.MessageData.Companion.message
+import io.github.rothes.esu.core.coroutine.IOScope
 import io.github.rothes.esu.core.module.Feature
 import io.github.rothes.esu.core.module.configuration.BaseModuleConfiguration
 import io.github.rothes.esu.core.module.configuration.EmptyConfiguration
 import io.github.rothes.esu.lib.configurate.yaml.YamlConfigurationLoader
 import io.github.rothes.esu.velocity.module.AutoReloadExtensionPluginsModule.ModuleConfig
 import io.github.rothes.esu.velocity.plugin
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import net.frankheijden.serverutils.velocity.managers.VelocityPluginManager
 import kotlin.jvm.optionals.getOrNull
+import kotlin.time.Duration.Companion.seconds
 
 object AutoReloadExtensionPluginsModule: VelocityModule<ModuleConfig, EmptyConfiguration>() {
 
@@ -35,17 +40,33 @@ object AutoReloadExtensionPluginsModule: VelocityModule<ModuleConfig, EmptyConfi
         if (!plugin.enabledHot)
             return
 
-        for (plugin in toLoad) {
+        for (pl in toLoad) {
             val serverUtils = VelocityPluginManager.get()
-            serverUtils.getPluginFile(plugin).getOrNull()?.let {
-                serverUtils.loadPlugin(it)
+            serverUtils.getPluginFile(pl).getOrNull()?.let {
+                val load = serverUtils.loadPlugin(it)
+                if (!load.isSuccess) {
+                    plugin.warn("[AutoReloadExtensionPlugins] Failed to load plugin $pl: ${load.result.name.lowercase()}")
+                    continue
+                }
+                runBlocking {
+                    // serverUtils.enablePlugin infinite loop, so we don't know if it's completed
+                    // https://github.com/FrankHeijden/ServerUtils/pull/72#issuecomment-2577691669
+                    IOScope.launch {
+                        val enable = serverUtils.enablePlugin(load.plugin)
+                        if (!enable.isSuccess) {
+                            plugin.warn("[AutoReloadExtensionPlugins] Failed to enable plugin $pl: ${load.result.name.lowercase()}")
+                        }
+                    }
+                    delay(0.5.seconds)
+                    plugin.info("[AutoReloadExtensionPlugins] Load plugin $pl")
+                }
             }
         }
     }
 
     override fun onDisable() {
         super.onDisable()
-        if (plugin.enabled)
+        if (plugin.enabled || !plugin.disabledHot)
             return
         val esuId = plugin.container.description.id
         val plugins = plugin.server.pluginManager.plugins.filter {
@@ -57,9 +78,21 @@ object AutoReloadExtensionPluginsModule: VelocityModule<ModuleConfig, EmptyConfi
             val otherId = db.id
             if (da.dependencies.any { it.id == otherId }) -1 else 1
         }
-        plugins.forEach {
-            VelocityPluginManager.get().unloadPlugin(it)
-            data.pluginsToLoad.add(it.description.id)
+        for (pl in plugins) {
+            val serverUtils = VelocityPluginManager.get()
+            val disable = serverUtils.disablePlugin(pl)
+            if (!disable.isSuccess) {
+                plugin.warn("[AutoReloadExtensionPlugins] Failed to disable plugin ${pl.description.id}: ${disable.result.name.lowercase()}")
+                continue
+            }
+            val unload = serverUtils.unloadPlugin(disable.plugin)
+            if (!unload.isSuccess) {
+                plugin.warn("[AutoReloadExtensionPlugins] Failed to unload plugin ${pl.description.id}: ${disable.result.name.lowercase()}")
+                continue
+            }
+
+            data.pluginsToLoad.add(pl.description.id)
+            plugin.info("[AutoReloadExtensionPlugins] Unloaded plugin ${pl.description.id}")
         }
         data.pluginsToLoad.reverse()
         ConfigLoader.save(dataPath, data)
