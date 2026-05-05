4d2a40d9f752ec57c578a89e59b53b42efacf008
Rothes <449181985@qq.com>
2026-05-05 14:00:21 +0800

    Fix 26.1 Support

--- bukkit/version/v26_1_2/src/main/java/io/github/rothes/esu/bukkit/module/essentialcommands/v26_1/AllocatingRateLimiterHandlerImpl.kt
+++ bukkit/version/v26_1_2/src/main/java/io/github/rothes/esu/bukkit/module/essentialcommands/v26_1/AllocatingRateLimiterHandlerImpl.kt
@@ -1,18 +1,19 @@
 package io.github.rothes.esu.bukkit.module.essentialcommands.v26_1
 
+import ca.spottedleaf.moonrise.common.misc.StaggeredRateLimiter
 import io.github.rothes.esu.bukkit.module.essentialcommands.PlayerChunkTickets
 import io.github.rothes.esu.core.util.ReflectionUtils.handle
 
 object AllocatingRateLimiterHandlerImpl : PlayerChunkTickets.AllocatingRateLimiterHandler {
 
     private val PREVIEW_ALLOCATION = let {
-        val clazz = Class.forName("ca.spottedleaf.moonrise.common.misc.AllocatingRateLimiter")
-        clazz.declaredMethods.first { it.name == "previewAllocation" }.handle
+        StaggeredRateLimiter::class.java.declaredMethods.first { it.name == "previewAllocation" }.handle
     }
 
     override fun previewAllocation(
         rateLimiter: Any, type: PlayerChunkTickets.AllocatingRateLimiterHandler.Type
     ): Long {
+        rateLimiter as StaggeredRateLimiter
         return PREVIEW_ALLOCATION.invokeExact(rateLimiter, getMaxRate(type).toLong()) as Long
     }
 
