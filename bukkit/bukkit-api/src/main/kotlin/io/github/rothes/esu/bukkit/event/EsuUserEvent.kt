d4622c0539700e270a2bb587b738380e31439eb8
Rothes <449181985@qq.com>
2026-05-15 10:49:39 +0800

    Change super class to Event `PlayerEvent(Player, boolean)` is package-protected on Spigot

--- bukkit/bukkit-api/src/main/kotlin/io/github/rothes/esu/bukkit/event/EsuUserEvent.kt
+++ bukkit/bukkit-api/src/main/kotlin/io/github/rothes/esu/bukkit/event/EsuUserEvent.kt
@@ -3,9 +3,9 @@ package io.github.rothes.esu.bukkit.event
 import io.github.rothes.esu.bukkit.user
 import io.github.rothes.esu.bukkit.user.PlayerUser
 import org.bukkit.entity.Player
-import org.bukkit.event.player.PlayerEvent
+import org.bukkit.event.Event
 
-abstract class EsuUserEvent(player: Player, async: Boolean = false) : PlayerEvent(player, async) {
+abstract class EsuUserEvent(val player: Player, async: Boolean = false) : Event(async) {
 
     val user: PlayerUser by lazy {
         player.user
