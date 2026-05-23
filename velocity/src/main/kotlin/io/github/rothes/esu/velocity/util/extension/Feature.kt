2c11fe6abda0ce4a45018d7bd5d176e1f4a04f83
Rothes <449181985@qq.com>
2026-05-23 13:46:11 +0800

    Sync esu-bukkit changes

--- velocity/src/main/kotlin/io/github/rothes/esu/velocity/util/extension/Feature.kt
+++ velocity/src/main/kotlin/io/github/rothes/esu/velocity/util/extension/Feature.kt
@@ -20,10 +20,10 @@ package io.github.rothes.esu.velocity.util.extension
 
 import io.github.rothes.esu.core.configuration.data.MessageData.Companion.message
 import io.github.rothes.esu.core.module.Feature
+import io.github.rothes.esu.core.module.Feature.AvailableCheck.Companion.errFail
 import io.github.rothes.esu.velocity.plugin
 
 fun Feature<*, *>.checkPacketEvents(): Feature.AvailableCheck? {
     if (plugin.server.pluginManager.getPlugin("packetevents").isPresent) return null
-    plugin.err("[$name] This feature requires packetevents plugin!")
-    return Feature.AvailableCheck.fail { "This feature requires packetevents plugin".message }
+    return errFail { "This feature requires packetevents plugin".message }
 }
