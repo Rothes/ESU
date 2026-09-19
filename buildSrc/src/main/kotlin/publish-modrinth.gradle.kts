eaa3e2da7c7785a6ad7d82114ad29b1fe5408a27
Rothes <449181985@qq.com>
2026-09-19 21:53:32 +0800

    Mark 26.3 as supported

--- buildSrc/src/main/kotlin/publish-modrinth.gradle.kts
+++ buildSrc/src/main/kotlin/publish-modrinth.gradle.kts
@@ -57,6 +57,7 @@ project.modrinth {
     this.changelog.set(changelog)
     versionType.set(if (isRelease) "release" else "alpha")
     gameVersions = listOf(
+        "26.3",
         "26.2",
         "26.1", "26.1.1", "26.1.2",
         "1.21", "1.21.1", "1.21.2", "1.21.3", "1.21.4", "1.21.5", "1.21.6", "1.21.7", "1.21.8", "1.21.9", "1.21.10", "1.21.11",
