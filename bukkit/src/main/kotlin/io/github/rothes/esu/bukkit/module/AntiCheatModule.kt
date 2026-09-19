64aa6d6c608ea7ceb19f7340f36904641ad0a026
Rothes <449181985@qq.com>
2026-09-20 00:24:32 +0800

    [AntiCheatModule] Move `NoClientTeleportConfirm` below `Exploit`

--- bukkit/src/main/kotlin/io/github/rothes/esu/bukkit/module/AntiCheatModule.kt
+++ bukkit/src/main/kotlin/io/github/rothes/esu/bukkit/module/AntiCheatModule.kt
@@ -1,12 +1,12 @@
 package io.github.rothes.esu.bukkit.module
 
-import io.github.rothes.esu.bukkit.module.anticheat.NoClientTeleportConfirm
+import io.github.rothes.esu.bukkit.module.anticheat.ExploitCheats
 import io.github.rothes.esu.core.module.configuration.BaseModuleConfiguration
 
 object AntiCheatModule : BukkitModule<BaseModuleConfiguration, Unit>() {
 
     init {
-        registerFeature(NoClientTeleportConfirm)
+        registerFeature(ExploitCheats)
     }
 
     override fun onEnable() {}
