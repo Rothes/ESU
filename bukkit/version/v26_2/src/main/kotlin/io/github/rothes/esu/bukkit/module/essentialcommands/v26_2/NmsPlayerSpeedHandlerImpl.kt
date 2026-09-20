0aad05b9e77a28a55f074a1d2057980066f8e00c
Rothes <449181985@qq.com>
2026-09-20 10:27:24 +0800

    Minecraft 26.2 NmsPlayerSpeedHandler

---
+++ bukkit/version/v26_2/src/main/kotlin/io/github/rothes/esu/bukkit/module/essentialcommands/v26_2/NmsPlayerSpeedHandlerImpl.kt
@@ -0,0 +1,46 @@
+/*
+ * This file is part of ESU - https://github.com/Rothes/ESU
+ * Copyright (C) 2026 Rothes & contributors
+ *
+ * ESU is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * ESU is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with ESU. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package io.github.rothes.esu.bukkit.module.essentialcommands.v26_2
+
+import io.github.rothes.esu.bukkit.module.essentialcommands.Speed
+import io.github.rothes.esu.bukkit.util.version.adapter.AttributeAdapter
+import org.bukkit.craftbukkit.entity.CraftPlayer
+import org.bukkit.entity.Player as BukkitPlayer
+
+// Compare with 1.17.1: walkingSpeed and flyingSpeed are now getter/setter since this version.
+object NmsPlayerSpeedHandlerImpl: Speed.NmsPlayerSpeedHandler {
+
+    override fun getWalkSpeed(bukkitPlayer: BukkitPlayer) = bukkitPlayer.nms.abilities.walkingSpeed
+    override fun getFlySpeed(bukkitPlayer: BukkitPlayer) = bukkitPlayer.nms.abilities.flyingSpeed
+
+    override fun setWalkSpeed(bukkitPlayer: BukkitPlayer, speed: Float) {
+        val player = bukkitPlayer.nms
+        player.abilities.walkingSpeed = speed
+        player.onUpdateAbilities()
+        bukkitPlayer.getAttribute(AttributeAdapter.MOVEMENT_SPEED)!!.baseValue = speed.toDouble()
+    }
+    override fun setFlySpeed(bukkitPlayer: BukkitPlayer, speed: Float) {
+        val player = bukkitPlayer.nms
+        player.abilities.flyingSpeed = speed
+        player.onUpdateAbilities()
+    }
+
+    private val BukkitPlayer.nms
+        get() = (this as CraftPlayer).handle
+}
